11c8153e08b74fa8feb7e2d34769d1de6929f7c0
bala-amypo <bala@amypo.in>
2025-12-20 17:39:38 +0000

    User commit

--- src/main/java/com/example/demo/controller/DeviceProfileController.java
+++ src/main/java/com/example/demo/controller/DeviceProfileController.java
@@ -2,15 +2,10 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.DeviceProfile;
 import com.example.demo.service.DeviceProfileService;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
-@RestController
 public class DeviceProfileController {
-     @Autowired
     private final DeviceProfileService deviceService;
 
     public DeviceProfileController(DeviceProfileService deviceService) {
