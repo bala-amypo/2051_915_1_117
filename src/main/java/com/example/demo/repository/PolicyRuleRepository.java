be18bc2bf7c7b539553410024bb74bf94dc717cd
bala-amypo <bala@amypo.in>
2025-12-21 08:48:31 +0000

    User commit

--- src/main/java/com/example/demo/repository/PolicyRuleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.PolicyRule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PolicyRuleRepository extends JpaRepository<PolicyRule, Long> {
-}
