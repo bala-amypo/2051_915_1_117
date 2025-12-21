576c89a6437cb31d9d2d4cbc85dcbe887186ff70
bala-amypo <bala@amypo.in>
2025-12-21 06:16:19 +0000

    User commit

--- src/main/java/com/example/demo/entity/LoginEvent.java
+++ src/main/java/com/example/demo/entity/LoginEvent.java
@@ -4,7 +4,28 @@ import lombok.Getter;
 import lombok.Setter;
 import java.time.LocalDateTime;
 
-@Entity @Table(name = "login_events") @Getter @Setter
+@Entity
+@Table(name = "login_events")
+@Getter
+@Setter
 public class LoginEvent {
-    @Id @GeneratedValue private Long id;
-    @Column(name
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "user_id")
+    private Long userId;
+    
+    @Column(name = "ip_address", nullable = false)
+    private String ipAddress;
+    
+    private String location;
+    
+    @Column(name = "device_id", nullable = false)
+    private String deviceId;
+    
+    private LocalDateTime timestamp = LocalDateTime.now();
+    
+    @Column(name = "login_status")
+    private String loginStatus;
+}
